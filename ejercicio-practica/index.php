5dab8bc6c80fb0aa5ce41ba4d74392cfd50f52db
Pablo Ramírez <137399638+Pablorpl1998@users.noreply.github.com>
2023-06-22 12:39:47 +0200

    initial commit

---
+++ ejercicio-practica/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta name="author" content="">
+    <meta name="copyright" content="">
+    <meta name="contact" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="robots" content="NoIndex, NoFollow">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="icon/png" href="favicon.png">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index-style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <title>Portfolio</title>
+</head>
+
+<body>
+    <div class="block-left-fixed">
+        <div class="img-block-fixed">
+            <img src="rsc/img/rs-logoo.png" alt="">
+        </div>
+        <div class="espacio-web-fixed"></div>
+        <div class="menu-web-fixed" id="about-menu"><a>
+                <h3>About</h3>
+            </a></div>
+        <div class="menu-web-fixed">
+            <h3>Skills</h3>
+        </div>
+        <div class="menu-web-fixed">
+            <h3>Works</h3>
+        </div>
+        <div class="menu-web-fixed">
+            <h3>Contact</h3>
+        </div>
+        <div class="menu-web-fixed">
+            <h3>Our</h3>
+        </div>
+        <div class="social-media-fixed">
+            <i class="fa-brands fa-facebook-f" id="rrss"></i>
+            <i class="fa-brands fa-instagram" id="rrss"></i>
+            <i class="fa-brands fa-twitter" id="rrss"></i>
+            <i class="fa-brands fa-linkedin-in" id="rrss"></i>
+        </div>
+    </div>
+
+    <div class="block-rest">
+        <div class="contain-block-rest-left">
+            <h1>
+                <p class="animate__animated animate__fadeIn">Hi,</p> I'm <span class="pe">P</span>ablo and <p
+                    style="color:var(--terciario);" class="animate__animated animate__fadeInLeft">i'm web</p>
+                <p class="pe">design</p>
+            </h1>
+            <h4><span class="pe">F</span>rontEnd <span class="pe">D</span>eveloper / <span class="pe">W</span>ordpress
+                <span class="pe">E</span>xpert
+            </h4>
+        </div>
+
+        <div class="contain-block-rest-right">
+            <div class="space-bottom">
+                <button class='glowing-btn'><span class='glowing-txt'>H<span
+                            class='faulty-letter'>E</span>LP</span></button>
+            </div>
+            <img src="rsc/img/abstract.webp" alt="">
+        </div>
+    </div>
+    <div class="block-body-about" id="about">
+        <h2>About</h2>
+        <hr>
+        <div class="skill-about">
+            <div class="frame-skill-type" id="frame-skill-type">
+                <i class="fa-solid fa-bolt" class="icon-skill"></i>
+                <h3>Fast</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure facere optio qui quam tempore expedita
+                </p>
+            </div>
+            <div class="frame-skill-type" class="card">
+                <i class="fa-solid fa-computer" class="icon-skill"></i>
+                <h3>Responsive</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure facere optio qui quam tempore expedita
+                </p>
+            </div>
+            <div class="frame-skill-type" class="card">
+                <i class="fa-regular fa-lightbulb" class="icon-skill"></i>
+                <h3>Intuitive</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure facere optio qui quam tempore expedita
+                </p>
+            </div>
+            <div class="frame-skill-type" class="card">
+                <i class="fa-solid fa-rocket" class="icon-skill"></i>
+                <h3>Dynamic</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure facere optio qui quam tempore expedita
+                </p>
+            </div>
+        </div>
+        <div class="block-body-skill">
+            <h2>Skills</h2>
+            <hr id="raya-skill" class="ralla">
+            <div class="block-skill-left">
+                <img src="rsc/img/foto-mia.jpeg" alt="" class="foto-mia">
+                <h3>Who i am?</h3>
+                <p>I'm a <span class="pe">Front-End Developer</span> for ChowNow in Los Angeles, CA.
+                    I have serious passion for <span class="pe">UI effects</span>, animations and creating intuitive,
+                    dynamic user experiences.
+                    Let's make something special.</p>
+            </div>
+            <div class="block-skill-right">
+                <div class="block-barra-skill">
+                    <div class="barrita-skill">
+                        <div class="nombre-skill-barra">
+                            <h3>CSS</h3>
+                        </div>
+                        <div class="porcentaje-skill-barra porcen-css-skill"></div>
+                        <div class="numero-skill-barra numero-css-skill">
+                            <h3>90% </h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-barra-skill">
+                    <div class="barrita-skill">
+                        <div class="nombre-skill-barra">
+                            <h3>HTML</h3>
+                        </div>
+                        <div class="porcentaje-skill-barra porcen-html-skill"></div>
+                        <div class="numero-skill-barra numero-html-skill">
+                            <h3>90%</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-barra-skill">
+                    <div class="barrita-skill">
+                        <div class="nombre-skill-barra wordpress-skill">
+                            <h3>WORDPRESS</h3>
+                        </div>
+                        <div class="porcentaje-skill-barra porcen-wordp-skill"></div>
+                        <div class="numero-skill-barra numero-wordp-skill">
+                            <h3>70%</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-barra-skill">
+                    <div class="barrita-skill">
+                        <div class="nombre-skill-barra javas-skill">
+                            <h3>JAVASCRIPT</h3>
+                        </div>
+                        <div class="porcentaje-skill-barra porcen-javas-skill"></div>
+                        <div class="numero-skill-barra numero-javas-skill">
+                            <h3>45%</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="block-barra-skill">
+                    <div class="barrita-skill">
+                        <div class="nombre-skill-barra php-skill">
+                            <h3>PHP</h3>
+                        </div>
+                        <div class="porcentaje-skill-barra porcen-php-skill"></div>
+                        <div class="numero-skill-barra numero-php-skill">
+                            <h3>50%</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script>
+
+
+        //*************************
+        $("#about-menu").click(function () {
+            $(".block-body-about").css("display", "block");
+        });
+
+
+
+    </script>
+</body>
+
+</html>
